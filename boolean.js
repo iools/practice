d907109cf76dd82159911ea7794cef71339f1391
Haozhi Chen <haozhichen31@gmail.com>
2023-07-01 14:36:54 -0500

    add grade checker fucntion

--- boolean.js
+++ boolean.js
@@ -7,3 +7,21 @@ function greaterThan100(number) {
 }
 
 greaterThan100(98)
+
+function checkGrade(score) {
+    if (score >= 90) {
+        return("A")
+    }else if (score>=80){
+        return("B")
+    }
+    else if (score>=70){
+        return("C")
+    }
+    else if (score>=60){
+        return("D")
+    }
+    else{
+        return("F")
+    }
+} 
+console.log(checkGrade(89))
