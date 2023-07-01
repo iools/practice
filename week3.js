c7c2df2a7203168d0eef51f3f2da6a3246eb9479
Haozhi Chen <haozhichen31@gmail.com>
2023-07-01 13:32:03 -0500

    practice questions 1-3

---
+++ week3.js
@@ -0,0 +1,6 @@
+
+
+const studentId = '1234' 
+const studentName = "Haozhi"
+const schoolName = "CPS"
+console.log("My name is " + studentName + ". My student ID is " + studentId + " and I go to " + schoolName + ".")     
