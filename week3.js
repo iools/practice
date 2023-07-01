ff47b85d6eb4e8024cf17a16687dc7803df8ad85
Haozhi Chen <haozhichen31@gmail.com>
2023-07-01 13:52:12 -0500

    add area function

--- week3.js
+++ week3.js
@@ -1,6 +1,19 @@
+//question 1
+const number =1
+console.log(number)
 
+//question 2
+let length = 10
+console.log(length + 15)
 
+//question 3
 const studentId = '1234' 
 const studentName = "Haozhi"
 const schoolName = "CPS"
 console.log("My name is " + studentName + ". My student ID is " + studentId + " and I go to " + schoolName + ".")     
+
+//question 5
+function area (radius){
+return 3.14 * radius * radius
+}
+console.log(area(1))
