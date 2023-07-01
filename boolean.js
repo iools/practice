0ee7d0f588f579399526bfc63d2996842f960875
Haozhi Chen <haozhichen31@gmail.com>
2023-07-01 14:08:33 -0500

    boolean function

---
+++ boolean.js
@@ -0,0 +1,9 @@
+function greaterThan100(number) {
+    if (number > 100) {
+        console.log("greater than 100")
+    } else{
+        console.log("not greater than 100")
+    }
+}
+
+greaterThan100(98)
